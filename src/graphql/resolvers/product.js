b3db568d337df50deea583d0dea09f48813cf450
jungRoit <roit.github@gmail.com>
2021-01-28 23:54:26 +0545

    003 added updateProduct mutation to update product

--- src/graphql/resolvers/product.js
+++ src/graphql/resolvers/product.js
@@ -10,6 +10,17 @@ const resolvers = {
       const product = new Product(object);
 
       return product.save();
+    },
+    updateProduct: async (parent, object) => {
+      const product = await Product.findOne({_id:object.id});
+      console.log('prod', product);
+      if(!product) {
+        throw new ProductNotFoundError(`Product with ID ${object.id} not found.`)
+      }
+
+      await Product.updateOne({_id:object.id},object);
+      return await Product.findOne({_id:object.id});
+
     }
   }
 };
--- src/graphql/schemas/product.js
+++ src/graphql/schemas/product.js
@@ -8,10 +8,13 @@ const productSchema = gql`
     imageUrl: String
   },
   type Query {
-    Products: [Product]
+    Products: [Product],
+    ProductNotFoundError: String
   },
   type Mutation {
     addProduct(name: String!, price: Float!, imageUrl: String!): Product,
+    updateProduct(id:ID!, name: String, price: Float, imageUrl: String): Product,
+    deleteProduct(id:ID!): Boolean
   }`;
 
   module.exports = productSchema;
